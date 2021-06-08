cf1d8e3a350b0e4ad3cad1cd64ab2f87de6b4399
msrd0 <msrd0@users.noreply.github.com>
2021-06-08 22:21:22 +0000

    GitHub Pages for 9c11e6b9590d008692bc8bb2c7831a0ce833ebea

--- doc/implementors/embedded_graphics/text/renderer/trait.TextRenderer.js
+++ doc/implementors/embedded_graphics/text/renderer/trait.TextRenderer.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
-implementors["bitmap_font"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"embedded_graphics/text/renderer/trait.TextRenderer.html\" title=\"trait embedded_graphics::text::renderer::TextRenderer\">TextRenderer</a> for <a class=\"struct\" href=\"bitmap_font/struct.BitmapFont.html\" title=\"struct bitmap_font::BitmapFont\">BitmapFont</a>&lt;'a&gt;","synthetic":false,"types":["bitmap_font::BitmapFont"]}];
+implementors["bitmap_font"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"embedded_graphics/text/renderer/trait.TextRenderer.html\" title=\"trait embedded_graphics::text::renderer::TextRenderer\">TextRenderer</a> for <a class=\"struct\" href=\"bitmap_font/struct.TextStyle.html\" title=\"struct bitmap_font::TextStyle\">TextStyle</a>&lt;'a&gt;","synthetic":false,"types":["bitmap_font::TextStyle"]}];
 implementors["embedded_graphics"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
